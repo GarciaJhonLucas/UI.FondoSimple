09a8cb4f2ed0cd1007a05c947fc12156ec0044f8
Garcia Jhon Lucas <garciajhonlucas@gmail.com>
2023-12-09 12:08:41 -0500

    componente de cliente

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ClientesComponent } from './clientes/clientes.component';
+import { FormularioClientesComponent } from './clientes/formulario-clientes/formulario-clientes.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ClientesComponent,
+    FormularioClientesComponent
+  ],
   imports: [
     CommonModule,
   ]
